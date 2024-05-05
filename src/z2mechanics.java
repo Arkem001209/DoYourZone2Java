62b55b7724cd5dd22d40c66754a91b21e10b1b92
Max Arkell <maxarkell@icloud.com>
2024-05-05 15:38:36 +0100

    first commit

---
+++ src/z2mechanics.java
@@ -0,0 +1,18 @@
+public class z2mechanics {
+    //main parameters for user and fitness level
+    private String username;
+    private int maxHR;
+    private int restinghr;
+    private int lowerZone2Range;
+    private int upperZone2Range;
+    private int totalExperiencePoints;
+    public z2mechanics(String Jim){
+        username = "Jim";
+        maxHR = 200;
+        restinghr = 45;
+        lowerZone2Range = 120;
+        upperZone2Range = 155;
+        totalExperiencePoints = 1;
+    }
+    
+}
