c6440e24269b9419c3e085ac3456eff698c3f8eb
Max Arkell <maxarkell@icloud.com>
2024-05-05 16:03:07 +0100

    initialise main application

--- src/z2app.java
+++ src/z2app.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class z2app {
+    public static void main(String[] args){
+        int select;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to DoYourZone2 V1.0! Use this app to track your zone 2. With each successful zone 2 workout you will get stronger! Enter your username to continue:");
+        String username = input.nextLine();
+
+        z2mechanics athlete = new z2mechanics(username);
+    }
 }
