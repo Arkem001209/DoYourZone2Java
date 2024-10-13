3ce10fe9769a4045be5ce4d4cf580015545855fd
Max Arkell <maxarkell@icloud.com>
2024-10-13 17:38:45 +1100

    write connection code

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -1,3 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private static final String URL = "/usr/local/mysql";
+    private static final String USER = "******";
+    //to do - create user that isnt root
+    private static final String PASSWORD = "******";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 
 }
